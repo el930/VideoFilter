f8472432a479b600501ece66dfafa9a6c90a51c7
el930 <elouie930@student.fuhsd.org>
2023-01-13 10:49:15 -0800

    nerd

--- src/RunMe.java
+++ src/RunMe.java
@@ -2,6 +2,7 @@ import core.DisplayWindow;
 
 public class RunMe {
     public static void main(String[] args) {
+        int nerd;
         // --== Load an image to filter ==--
         DisplayWindow.showFor("images/6.jpg", 800, 600);
 
